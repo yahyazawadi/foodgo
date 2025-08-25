b77d8f40ee0a12bb0a3b045d4712708213c01976
Yahyazawadi <yahyazawadi278@gmail.com>
2025-08-25 12:38:30 +0300

    added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dofood/pages/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,50 +6,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ super.key });
-  
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "foodGo",
       theme: ThemeData(
         primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      routes: 
-  {
-        '/' :(context) => HomeScreen(),
-         'products': (context) => ProductsPage(),
-         'product-details':(context) => ProductDetails(),
-         'cart': (context) => CartPage(),
-          'order-sumary': (context) => OrderSummary(),
-        // i do not know if I might need those 
+      routes: {
+        '/': (context) => HomeScreen(),
+        'products': (context) => ProductsPage(),
+        'product-details': (context) => ProductDetails(),
+        'cart': (context) => CartPage(),
+        'order-sumary': (context) => OrderSummary(),
+        // I do not know if I might need those but we'll see
         //   '/payment-methods': (context) => PaymentMethodsScreen(),
         // '/order-confirm': (context) => OrderConfirmationScreen(),
         // '/profile': (context) => ProfileScreen(),
         // '/support': (context) => SupportScreen(),
-      }
-    )
+      },
+    );
   }
-
- 
 }
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("HomeScreen")),
-      body: Center(
-        child: Text("home screen"),
-      ) 
-    ),),},}
-      
-    
-  
-  
-
-
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(child: Text("FoodGO")),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("home"),
+            onTap: () => _navigateTo(context, '/'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text("Products"),
+            onTap: () => _navigateTo(context, '/products'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text("Cart"),
+            onTap: () => _navigateTo(context, '/cart'),
+          ),
+          ListTile(
+            leading: Icon(Icons.note),
+            title: Text("order summary"),
+            onTap: () => _navigateTo(context, '/order-sumary'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _navigateTo(BuildContext context, String routeName) {
+    Navigator.pop(context);
+
+    if (ModalRoute.of(context)?.settings.name != routeName) {
+      if (routeName == '/') {
+        Navigator.pushReplacementNamed(context, routeName);
+      } else {
+        Navigator.pushNamed(context, routeName);
+      }
+    }
+  }
+}
